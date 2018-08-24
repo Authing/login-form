d95a42375e0ce54f1a0de67d8a7ba2417256140d
ivy <597055914@qq.com>
2018-08-24 20:02:04 +0800

    Add DIY params

--- main.js
+++ main.js
@@ -45,6 +45,15 @@
   var AuthingForm = function (opts) {
 
     var 
+
+      PLACEHOLDER_TEXT = {
+        'USERNAME': '请输入用户名',
+        'EMAIL': '请输入邮箱',
+        'PASSWORD': '请输入密码',
+        'CONFIRM_PASSWORD': '请确认密码',
+        'VERIFY_CODE': '请输入验证码',
+        'NEW_PASSWORD': '请输入新密码'
+      },
     
       loadVue = function (onload) {
         var vueScript = document.createElement('script');
@@ -88,6 +97,40 @@
       };
 
       $authing.opts = opts;
+
+      $authing.opts.hideQRCode = opts.hideQRCode || false;
+      $authing.opts.hideUP = opts.hideUP || false;
+      $authing.opts.hideOAuth = opts.hideOAuth || false;
+
+      // 初始化小程序扫码登录配置 
+      if(opts.qrcodeScanning) {
+        opts.qrcodeScanning.redirect = opts.qrcodeScanning.redirect || true;
+        opts.qrcodeScanning.interval = opts.qrcodeScanning.interval || 1500;
+        opts.qrcodeScanning.tips = opts.qrcodeScanning.tips || null;
+      }
+
+      // 初始化 placeholder
+      if (opts.placeholder) {
+        opts.placeholder.username = opts.placeholder.username || PLACEHOLDER_TEXT.USERNAME;
+        opts.placeholder.email = opts.placeholder.email || PLACEHOLDER_TEXT.EMAIL;
+        opts.placeholder.password = opts.placeholder.password || PLACEHOLDER_TEXT.PASSWORD;
+        opts.placeholder.confirmPassword = opts.placeholder.confirmPassword || PLACEHOLDER_TEXT.USERNAME;
+        opts.placeholder.verfiyCode = opts.placeholder.verfiyCode || PLACEHOLDER_TEXT.VERIFY_CODE;
+        opts.placeholder.newPassword = opts.placeholder.newPassword || PLACEHOLDER_TEXT.NEW_PASSWORD;
+      }else {
+        opts.placeholder = {
+          username: PLACEHOLDER_TEXT.USERNAME,
+          email: PLACEHOLDER_TEXT.EMAIL,
+          password: PLACEHOLDER_TEXT.PASSWORD,
+          confirmPassword: PLACEHOLDER_TEXT.CONFIRM_PASSWORD,
+          verfiyCode: PLACEHOLDER_TEXT.VERIFY_CODE,
+          newPassword: PLACEHOLDER_TEXT.NEW_PASSWORD  
+        }
+      }
+
+      $authing.opts.placeholder = opts.placeholder;
+
+      console.log($authing.opts);
 
     window.onload = function () {
     
@@ -149,7 +192,10 @@
   
             isWxQRCodeGenerated: false,
   
-            isScanCodeEnable: false
+            isScanCodeEnable: false,
+
+            opts: $authing.opts
+
           },
           created: function () {
             var that = this;
@@ -171,27 +217,32 @@
                 that.loginForm.password = that.decrypt(localStorage.getItem('_authing_password'), opts.clientId);
               }
 
-              that.oAuthloading = true;
-
-              validAuth.readOAuthList()
+              if(!that.opts.hideOAuth) {
+                that.oAuthloading = true;
+                validAuth.readOAuthList()
                 .then(function (data) {
                   $authing.pub('oauthLoad', data);
                   that.oAuthloading = false;
-
+  
                   var OAuthList = data.filter(function (item) {
                     if (item.alias === 'wxapp') {
                       that.isScanCodeEnable = true;
                     }
                     return item.enabled === true && item.alias !== 'wxapp';
                   });
-
+  
                   that.OAuthList = OAuthList;
-
+  
                 })
                 .catch(function (err) {
                   $authing.pub('oauthUnload', err);
                   that.oAuthloading = true;
-                });
+                });                
+              }else {
+                if(!that.opts.hideQRCode) {
+                  that.isScanCodeEnable = true;
+                }
+              }
             })
             .catch(function (err) {
               $authing.pub('authingUnload', err);
@@ -546,7 +597,7 @@
   
               if (!this.isWxQRCodeGenerated) {
                 validAuth.startWXAppScaning({
-                  mount: 'qrcode-node'
+                  mount: 'qrcode-node',
 
                   onSuccess: function(res) {
                     $authing.pub('scanning', res);
