fdb11c5a4f0a824a4069a6182f03ba0b758f42f5
Mark Yeh <yelexin@hotmail.com>
2018-08-25 16:16:29 +0800

    refact: shorten code

--- main.js
+++ main.js
@@ -260,12 +260,12 @@
               this.verifyCodeLoading = false;
               console.log('verifyCode loaded');
             },
-            encrypt: function encrypt(str, key) {
+            encryptMethod: function encryptMethod(str, key) {
               while (str.length > key.length) {
                 key += key;
               }
               var r = key.substr(0, str.length);
-  
+
               var i = 0;
               var arr = [];
               for (i = 0; i < str.length; i++) {
@@ -275,26 +275,15 @@
               arr.map(function (char) {
                 uglyStr += String.fromCharCode(char);
               });
-              var out = window.btoa(uglyStr);
+              return uglyStr
+            },
+            encrypt: function encrypt(str, key) {
+              var out = window.btoa(this.encryptMethod(str, key));
               return out;
             },
             decrypt: function decrypt(str, key) {
               str = window.atob(str);
-              while (str.length > key.length) {
-                key += key;
-              }
-              var r = key.substr(0, str.length);
-  
-              var i = 0;
-              var arr = [];
-              for (i = 0; i < str.length; i++) {
-                arr.push(str.charCodeAt(i) ^ r.charCodeAt(i));
-              }
-              var uglyStr = '';
-              arr.map(function (char) {
-                uglyStr += String.fromCharCode(char);
-              });
-              return uglyStr;
+              return this.encryptMethod(str, key)
             },
             removeGlobalMsg: function removeGlobalMsg() {
               this.warnVisible = false;
